9a851ec5f70717e6353d33bcf13460f2865c2d00
Rodrigo Campos <rodrigo.campos@noken.digital>
2021-10-08 12:10:55 -0300

    project

---
+++ Skimmer.Library/Divisible.cs
@@ -0,0 +1,27 @@
+﻿namespace Skimmer.Library
+{
+    public static class Divisible
+    {
+        public static string DivisibleNumbers(int number)
+        {
+            number = number + 1;
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                 return $"{number} Fizz Buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return  $"{number} Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return $"{number} Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+    }
+}
---
+++ Skimmer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Skimmer.Library;
+using System;
+
+namespace Skimmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                Console.WriteLine(Divisible.DivisibleNumbers(i));
+            }
+        }
+    }
+}
